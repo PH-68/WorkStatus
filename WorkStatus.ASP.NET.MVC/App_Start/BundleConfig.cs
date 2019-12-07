72a7793f978e8c7a101c4981cbcaf2c96bb8b87b
Poyi Hong <poyi.hong@outlook.com>
2019-12-07 18:31:53 +0800

    Added feature for work status

--- WorkStatus.ASP.NET.MVC/App_Start/BundleConfig.cs
+++ WorkStatus.ASP.NET.MVC/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace WorkStatus.ASP.NET.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.slim.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/chart").Include(
+                        "~/Scripts/Chart.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +23,13 @@ namespace WorkStatus.ASP.NET.MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css",
+                      "~/Content/chart.min.css"
+                      ));
         }
     }
 }
--- WorkStatus.ASP.NET.MVC/Controllers/HomeController.cs
+++ WorkStatus.ASP.NET.MVC/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using HtmlAgilityPack;
-using System;
-using System.Net;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace WorkStatus.ASP.NET.MVC.Controllers
 {
@@ -16,39 +13,18 @@ namespace WorkStatus.ASP.NET.MVC.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Status()
         {
             //ViewBag.Message = "Your application description page.";
-            WebClient webClient = new WebClient
+            Models.WorkStatusDataModel workStatusData = new Models.WorkStatusDataModel((int)Models.WorkStatusDataModel.Languages.Chinese);
+            foreach (var item in workStatusData.StatusData)
             {
-                Encoding = System.Text.Encoding.UTF8
-            };
-
-            if (UnixTime < (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds || false == true)
-            {
-                UnixTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds + 1800;
-                //html的node用大寫
-                string html = webClient.DownloadString("https://www.dgpa.gov.tw/typh/daily/nds.html").Replace("<FONT", "<SPAN").Replace("</FONT>", "</SPAN>");
-                string table = html.Substring(html.IndexOf("<TABLE"), html.IndexOf("</TABLE>") - html.IndexOf("<TABLE") + 8);
-                //xmlDocument.LoadXml(webClient.DownloadString("https://www.dgpa.gov.tw/typh/daily/nds.html"));
-                //HtmlWeb webClient = new HtmlWeb();
-                HtmlTable = table;
-                html = webClient.DownloadString("https://www.dgpa.gov.tw/typh/daily/ndse.html").Replace("<FONT", "<SPAN").Replace("</FONT>", "</SPAN>");
-                table = html.Substring(html.IndexOf("<TABLE"), html.IndexOf("</TABLE>") - html.IndexOf("<TABLE") + 8);
-                EHtmlTable = table;
+                ViewBag.Message = item.Value;
             }
-            ViewBag.Message = EHtmlTable;
-            HtmlDocument doc = new HtmlDocument();
-            webClient.Headers["User-Agent"] = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
-            doc.LoadHtml(webClient.DownloadString("https://www.dgpa.gov.tw/typh/daily/ndse.html"));
-            HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@id='Table']/tbody[1]/tr[1]/td/font/h2");
-            if (node != null)
-            {
-                ViewBag.Message = node.InnerText;
-
-            }
+            ViewBag.Dictionary = workStatusData.StatusData;
+            ViewBag.IsClosuresWorking = workStatusData.IsClosuresWorking;
             return View();
-        }
+            }
 
         public ActionResult Contact()
         {
